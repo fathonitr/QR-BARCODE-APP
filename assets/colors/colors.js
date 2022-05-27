5adc2a4d5a00c9808575b5ed5076451bd0f976c9
Habib Farhan <habib.farhan@student.htw-berlin.de>
2022-05-27 10:19:09 +0000

    new commit for the startscreen without using npm

---
+++ assets/colors/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  black: '#171717',
+  white: '#FFFFFF',
+  blue: '#3F82EE',
+  gray: '#9F9E9E',
+};
+
+export default colors;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Onboard from './Onboard';
 import CameraGallery from './CameraGallery';
+
 const App = () => {
-  return <CameraGallery />;
+  const [showOnboard, setShowOnboard] = useState(true);
+
+  const handleOnboardFinish = () => {
+    setShowOnboard(false);
+  };
+
+  return (
+    <>
+      {showOnboard ? (
+        <Onboard handleDone={handleOnboardFinish} />
+      ) : (
+        <CameraGallery />
+      )}
+    </>
+  );
 };
 
 export default App;
